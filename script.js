540171a8fdcc20fca7cae075e4f9898061078482
febyputra <93692256+febyputra@users.noreply.github.com>
2023-04-18 12:16:09 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -21,10 +21,9 @@ function showSlides(n) {
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
 }
-// Auto Slide 
+// Auto Slide
 var slideIndex = 0;
 showSlides();
-
 function showSlides() {
     var i;
     var slides = document.getElementsByClassName("mySlides");
@@ -34,5 +33,5 @@ function showSlides() {
     slideIndex++;
     if (slideIndex > slides.length) { slideIndex = 1 }
     slides[slideIndex - 1].style.display = "block";
-    setTimeout(showSlides, 3000); // Change image every 2 seconds
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
